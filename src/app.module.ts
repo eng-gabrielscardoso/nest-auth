07bf6d13a2b083aaed46b353c499e2d469d3ac57
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-03-24 23:59:05 -0300

    feat: reorganise modules and init auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './http/users/users.module';
+import { AuthModule } from './http/auth/auth.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { UsersModule } from './users/users.module';
       }),
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
